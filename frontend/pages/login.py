5bd970187184c2bfb80e62e1162da08f6c9d76f9
Yuvraj Singh Shekhawat <yuvrajshekhawat405@gmail.com>
2026-06-16 10:07:29 +0530

    Update login API URL from env

--- frontend/pages/login.py
+++ frontend/pages/login.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
 
-API_URL = "http://localhost:8000/auth"
+import os
+API_URL = os.getenv("API_URL_AUTH", "http://localhost:8000/auth")
 
 
 def render_login_page():
